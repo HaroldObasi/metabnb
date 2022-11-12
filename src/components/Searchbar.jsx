32e973953662944384972011b5c1acc545156c50
HaroldObasi <haroldobasi2k16@gmail.com>
2022-11-12 15:19:30 +0100

    responsive styling changes

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 function Searchbar() {
     return (
-        <div class="p-2 relative mx-auto">
+        <div class="py-2 relative ">
             <input
-                class="border border-gray-300 h-10 px-5 pr-16 rounded-l-lg text-sm focus:outline-none"
+                class="border h-10 pl-2 sm:pl-5 lg:pr-16 rounded-l-lg text-sm focus:outline-none"
                 placeholder="Search for"
             />
-            <button class="mr-4 px-20 py-2 purpleGradient text-[16px] rounded-r-lg  text-white">
+            <button class="inline-block mr-4 px-3 lg:px-20 py-2 purpleGradient text-[14px] sm:text-[16px] rounded-r-lg  text-white">
                 Submit
             </button>
         </div>
