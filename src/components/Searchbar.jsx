cdc478491b51c2d756558a2318c593f57a251a2e
HaroldObasi <haroldobasi2k16@gmail.com>
2022-11-15 12:16:26 +0100

    responsive changes

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -4,10 +4,10 @@ function Searchbar() {
     return (
         <div class="py-2 relative ">
             <input
-                class="border h-10 pl-2 sm:pl-5 lg:pr-16 rounded-l-lg text-sm focus:outline-none"
+                class="border w-[190px] lg:w-[350px] h-10 pl-2 sm:pl-5 lg:pr-16 rounded-l-lg text-sm focus:outline-none"
                 placeholder="Search for"
             />
-            <button class="inline-block mr-4 px-3 lg:px-20 py-2 purpleGradient text-[14px] sm:text-[16px] rounded-r-lg  text-white">
+            <button class="inline-block mr-4 px-10 lg:px-20 py-2 purpleGradient text-[14px] sm:text-[16px] rounded-r-lg  text-white">
                 Submit
             </button>
         </div>
