69a0f1fd58a546d3797dd80977d9b48f5a2616f8
HaroldObasi <haroldobasi2k16@gmail.com>
2022-11-15 12:15:44 +0100

    added places route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./views/Home";
-import Contact from "./views/Contact";
+import Places from "./views/Places";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 
@@ -11,7 +11,7 @@ function App() {
             <Router>
                 <Routes>
                     <Route exact path="/" element={<Home />} />
-                    <Route path="/contact" element={<Contact />} />
+                    <Route path="/places" element={<Places />} />
                 </Routes>
             </Router>
             <Footer />
