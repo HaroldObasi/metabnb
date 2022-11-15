de47bcb30bff4ab732ccbad376a17a9e4dc4e06f
HaroldObasi <haroldobasi2k16@gmail.com>
2022-11-15 12:15:56 +0100

    added image variables

--- src/constants.js
+++ src/constants.js
@@ -1,12 +1,48 @@
 import image_3 from "./assets/images/image_3.png";
 
+import place1 from "./assets/places/place1.png";
+import place2 from "./assets/places/place2.png";
+import place3 from "./assets/places/place3.png";
+import place4 from "./assets/places/place4.png";
+import place5 from "./assets/places/place5.png";
+import place6 from "./assets/places/place6.png";
+import place7 from "./assets/places/place7.png";
+import place8 from "./assets/places/place8.png";
+import place10 from "./assets/places/place10.png";
+import place11 from "./assets/places/place11.png";
+import place12 from "./assets/places/place12.png";
+import place13 from "./assets/places/place13.png";
+import place14 from "./assets/places/place14.png";
+import place15 from "./assets/places/place15.png";
+import place16 from "./assets/places/place16.png";
+import place17 from "./assets/places/place17.png";
+
 export const data = [
-    image_3,
-    image_3,
-    image_3,
-    image_3,
-    image_3,
-    image_3,
-    image_3,
-    image_3,
+    place1,
+    place2,
+    place3,
+    place4,
+    place5,
+    place6,
+    place7,
+    place8,
+];
+
+export const data2 = [
+    place1,
+    place2,
+    place3,
+    place4,
+    place5,
+    place6,
+    place7,
+    place8,
+    place10,
+    place11,
+    place12,
+    place13,
+    place14,
+    place15,
+    place16,
+    place17,
 ];
